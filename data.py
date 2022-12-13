dc91018dc377ca8b835d3c9c0444b197e2829a81
miguelgargallo <mgargallo@gmail.com>
2022-12-13 19:16:02 +0100

    1.0.6 Formmated

--- data.py
+++ data.py
@@ -9,25 +9,25 @@ data_folder = "pylar.data"
 
 # Check if the data folder exists, and create it if it doesn't
 if not os.path.exists(data_folder):
-  os.makedirs(data_folder)
+    os.makedirs(data_folder)
 
 # Loop through the rounds of the competition
 for round_number in range(1, 343+1):
-  round_url = f"{competition_url}/{round_number}"
-  
-  # Check if the round page has changed
-  page_content = requests.get(round_url).text
-  if not os.path.exists(f"{data_folder}/{round_number}.html"):
-    # If the file doesn't exist, download it
-    with open(f"{data_folder}/{round_number}.html", "w") as f:
-      f.write(page_content)
-  else:
-    # If the file already exists, check if it has been updated
-    with open(f"{data_folder}/{round_number}.html", "r") as f:
-      previous_content = f.read()
-    if page_content == previous_content:
-      continue
-    
-    # If the page has changed, update the file in the data folder
-    with open(f"{data_folder}/{round_number}.html", "w") as f:
-      f.write(page_content)
+    round_url = f"{competition_url}/{round_number}"
+
+    # Check if the round page has changed
+    page_content = requests.get(round_url).text
+    if not os.path.exists(f"{data_folder}/{round_number}.pylar"):
+        # If the file doesn't exist, download it
+        with open(f"{data_folder}/{round_number}.pylar", "w") as f:
+            f.write(page_content)
+    else:
+        # If the file already exists, check if it has been updated
+        with open(f"{data_folder}/{round_number}.pylar", "r") as f:
+            previous_content = f.read()
+        if page_content == previous_content:
+            continue
+
+        # If the page has changed, update the file in the data folder
+        with open(f"{data_folder}/{round_number}.pylar", "w") as f:
+            f.write(page_content)
