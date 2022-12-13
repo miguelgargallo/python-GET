6497e886d417eb11a8089aa02b1c56e7dfcb7550
miguelgargallo <mgargallo@gmail.com>
2022-12-13 19:27:27 +0100

    1.0.8 Readme

--- data.py
+++ data.py
@@ -1,6 +1,5 @@
 import requests
 import os
-import csv
 
 # URL of the competition rounds
 competition_url = "https://jutge.org/competitions/EDA:EDA_Q1_2022_23/rounds"
@@ -13,7 +12,7 @@ if not os.path.exists(data_folder):
     os.makedirs(data_folder)
 
 # Loop through the rounds of the competition
-for round_number in range(1, 343+1):
+for round_number in range(1, 242+1):
     round_url = f"{competition_url}/{round_number}"
 
     # Check if the round page has changed
@@ -32,11 +31,3 @@ for round_number in range(1, 343+1):
         # If the page has changed, update the file in the data folder
         with open(f"{data_folder}/{round_number}.pylar", "w") as f:
             f.write(page_content)
-
-    # Extract the table data from the page content
-    table_data = []
-    for line in page_content.split("\n"):
-        if "<td>" in line:
-            # Parse the line into a list of table cells
-            cells = [cell.strip() for cell in line.
-                    
