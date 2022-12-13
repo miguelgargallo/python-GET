1eb15e9250118345f010ec8f03b8cd28d25e20ee
miguelgargallo <mgargallo@gmail.com>
2022-12-13 20:13:40 +0100

    1.0.12 Added code and md files to document

--- cleaner.py
+++ cleaner.py
@@ -1,25 +1,20 @@
 import os
 
-# Path to the folder containing the files
-folder_path = "pylar.data"
+# Set the directory path
+directory = "pylar.data"
 
-# Get a list of all files in the folder
-file_list = os.listdir(folder_path)
+# Loop through all files in the directory
+for filename in os.listdir(directory):
+  # Open the file in read mode
+  with open(os.path.join(directory, filename), "r") as file:
+    # Read the first 188 lines and discard them
+    for _ in range(188):
+      file.readline()
 
-# Loop through each file in the folder
-for file_name in file_list:
-    # Full path to the file
-    file_path = os.path.join(folder_path, file_name)
+    # Read the rest of the file
+    data = file.read()
 
-    # Open the file in read mode
-    with open(file_path, "r") as file:
-        # Read the contents of the file
-        file_content = file.read()
-
-        # Clean the content inside the <head> tag
-        cleaned_content = file_content.replace("<head>", "").replace("</head>", "")
-
-    # Open the file in write mode
-    with open(file_path, "w") as file:
-        # Write the cleaned content to the file
-        file.write(cleaned_content)
+  # Open the file in write mode
+  with open(os.path.join(directory, filename), "w") as file:
+    # Overwrite the file with the remaining data
+    file.write(data)
